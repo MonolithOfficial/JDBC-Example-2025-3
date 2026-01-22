608d7f8835501ad05ff7643f3d3bb1432074ec5a
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2026-01-22 21:54:26 +0400

    add an example where querying happens in dataprovider method instead of steps

--- src/main/java/ge/tbc/testautomation/data/UserDataProvider.java
+++ src/main/java/ge/tbc/testautomation/data/UserDataProvider.java
@@ -2,6 +2,10 @@ package ge.tbc.testautomation.data;
 
 import org.testng.annotations.DataProvider;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDataProvider {
@@ -16,5 +20,39 @@ public class UserDataProvider {
         }
 
         return userData;
+    }
+
+    // DATABASE FETCHING IN DATAPROVIDER ITSELF
+    @DataProvider
+    public static Object[][] getUserData() {
+
+        try (Connection connection = MSSQLConnection.getConnection()) {
+            String SQL = """
+                    SELECT * FROM Users2
+                    WHERE Username = ? OR Username = ?
+                    """;
+            PreparedStatement preparedStatement
+                    = connection.prepareStatement(SQL, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            preparedStatement.setString(1, "standard_user");
+            preparedStatement.setString(2, "locked_out_user");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.last();
+            int rowCount = resultSet.getRow(); // getting row count
+            resultSet.beforeFirst();
+            Object[][] data = new Object[rowCount][resultSet.getMetaData().getColumnCount()];
+
+            int iter = 0;
+            while (resultSet.next()) {
+                data[iter] = new Object[]{
+                        resultSet.getString("Username"),
+                        resultSet.getString("PasswordHash")
+                };
+                iter++;
+            }
+            return data;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
